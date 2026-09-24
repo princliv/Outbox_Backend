d6477bb34f42a85312026fe319897978cbf4f834
Ankit Kumar <ankitkumar1990asap@gmail.com>
2026-09-24 15:10:23 +0530

    Messaging

---
+++ messaging_feature/services/SocketService.js
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+import { whiteListCors } from "../../config/cors.config.js";
+import mongoose from "mongoose";
+
+class SocketService {
+  constructor() {
+    this._io = new Server({
+      cors: {
+        origin: whiteListCors,
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
+    });
+  }
+
+  initListeners() {
+    try {
+      this._io.on("connection", (socket) => {
+        console.log("User connected:", socket.id);
+
+        socket.on("join", (userId) => {
+          try {
+            socket.join(userId);
+            console.log(`User ${userId} joined their room`);
+            socket.emit("joined", {
+              message: `Successfully joined room ${userId}`,
+              room: userId,
+            });
+          } catch (err) {
+            console.error(`Error in join event: ${err.message}`);
+          }
+        });
+
+        socket.on("disconnect", () => {
+          try {
+            console.log("User disconnected:", socket.id);
+          } catch (err) {
+            console.error(`Error in disconnect event: ${err.message}`);
+          }
+        });
+      });
+    } catch (err) {
+      console.error(`Error in initListeners: ${err.message}`);
+    }
+  }
+
+  emitToUser(userId, event, payload) {
+  console.log(`📣 Emitting '${event}' to room '${userId}'`);
+  this._io.to(userId.toString()).emit(event, payload);
+}
+
+}
+
+export default SocketService;
